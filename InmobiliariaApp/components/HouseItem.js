dddeddd02de4fc93ec26f59d9f7f0aa0654e0a81
yovanha <yfajardo0@ucol.mx>
2021-06-23 10:10:40 -0500

    Components added

--- InmobiliariaApp/components/HouseItem.js
+++ InmobiliariaApp/components/HouseItem.js
@@ -1,11 +1,38 @@
+import React from 'react';
 import {StyleSheet, View, Text, Image, TouchableHighlight} from 'react-native';
 
 export default function HouseItem({imageUri,name}) {
     return(
+        <View style = {styles.imgContainer}>
+            <TouchableHighlight>
+                <View>
+                    <Image source = {imageUri} style = {styles.images}/>
+                    <Text style = {styles.imageText}>
+                        {name}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        </View>
       );
 }
 
 const styles = StyleSheet.create({
-
+    imgContainer:{
+        width: 140,
+        paddingLeft: 10,
+        marginRight: 15,
+        paddingTop: 15
+    },
+    images:{
+        borderRadius: 10,
+        height: 140,
+        resizeMode:'cover',
+        width: '100%'
+    },
+    imageText:{
+        color: 'white',
+        fontSize: 20,
+        marginTop: 5
+    },
 
 });
